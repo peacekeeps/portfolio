5642a692c4e730b14fa407072f7df56d76d8e938
peacekeeps <app.juliog@gmail.com>
2023-04-17 22:25:34 -0300

    update

--- artebianco/node_405b364164943.tmp.js
+++ artebianco/node_405b364164943.tmp.js
@@ -1,5 +1,6 @@
 // let myStr = "Hello"
 
+
 // let newStr = "Hello"
 // let length = newStr.length
 
@@ -12,7 +13,7 @@
 //         setTimeout(function() {
 //             test(numb - 1, str)
 //         }, 500)
-
+        
 //     }
 // }
 
@@ -33,46 +34,53 @@
 
 // // test(length, newStr)
 
-// let firstStr = "Hello"
+// let firstStr = "Hello" 
 // let strLength = firstStr.length
 
 // test2(strLength, firstStr)
 
-let words = ["Hello", "Goodbye", "Welcome"];
+let words = [
+    "Hello",
+    "Goodbye",
+    "Welcome"
+]
 
-let blank = false;
+let blank = false
+
+console.log(words[0].length)
 
 function writer(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      setTimeout(() => {
-        wordPlay(arr[i], arr[i].length);
-      }, i * 4000); // espera 2 segundos antes de começar a escrever cada palavra
+    for (i = 0; i < arr.length; i++) {
+        wordPlay(arr[i], arr[i].length)
     }
-  }
+}
+
 function wordPlay(str, strLength) {
-  let string = "";
-  let interval = 500;
+    let string = ""
 
-  if (blank === false) {
-    if (strLength === 0) {
-      blank = true;
-      return string
+    if (blank === false) {
+        if (strLength === 0) {
+            blank = true
+            return string
+        } else {
+            string = str.slice(0, strLength)
+            console.log(string)
+            setTimeout(function() {
+                wordPlay(str, strLength - 1)
+            }, 500)
+        }
     } else {
-      string = str.slice(0, strLength);
-      console.log(string);
-      setTimeout(function () {
-        wordPlay(str, strLength - 1);
-      }, 500);
+        if (string.length === strLength) {
+            blank = false
+            return string
+        } else {
+            string = str.slice(0, string.length + 1)
+            console.log(string)
+            setTimeout(function() {
+                wordPlay(str, strLength + 1)
+            })
+        }
     }
-  } else {
-    for (let i = 0; i < str.length; i++) {
-        setTimeout(() => {
-          string += str[i];
-          console.log(string);
-        }, i * interval);
-      }
-    }
-    blank = false;
-  }
+}
 
-writer(words)
+writer(words) 
